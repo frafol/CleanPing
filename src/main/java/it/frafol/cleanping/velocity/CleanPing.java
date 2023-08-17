8752aaca5e2c966a8a06d2fdb97449b2709de4f8
Francesco <67754059+frafol@users.noreply.github.com>
2023-08-17 09:24:40 +0200

    Bump to 1.4.6

--- src/main/java/it/frafol/cleanping/velocity/CleanPing.java
+++ src/main/java/it/frafol/cleanping/velocity/CleanPing.java
@@ -36,7 +36,7 @@ import java.nio.file.StandardCopyOption;
 @Plugin(
 		id = "cleanping",
 		name = "CleanPing",
-		version = "1.4.5",
+		version = "1.4.6",
 		dependencies = {@Dependency(id = "redisbungee", optional = true)},
 		description = "Adds /ping command to check your and player's ping.",
 		authors = { "frafol" })
